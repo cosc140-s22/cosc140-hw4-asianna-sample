bc4139396f7f8932d7153aaa24593da9effd2d2e
Asianna Sample <99026080+Asianna01@users.noreply.github.com>
2022-05-31 11:52:52 +0000

    Homework 4

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Prefetch
+from .models import Product
 
-# Create your views here.
+
+def index(request):
+    products = Product.objects.all().order_by('name')
+    return render(request,
+                  "products/index.html",
+                  context={"products": products})
+
+
+def show(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, "products/show.html", context={"product": product})
